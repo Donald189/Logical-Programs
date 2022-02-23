b2ffc7a7eafa10a9d8cf94465cf509ec94d7460c
Donald189 <donaldkarey@gmail.com>
2022-02-23 13:48:53 +0530

    STOP WATCH

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+        Thread.Sleep(10000);
+        stopWatch.Stop();
+        
+        TimeSpan ts = stopWatch.Elapsed;
+
+        
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+        Console.WriteLine("RunTime " + elapsedTime);
+    }
+}
+
